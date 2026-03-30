5ee3d7e24d2a28b30ab2b517a960f2e60d84efa7
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-03-30 12:32:43 -0300

    first commit

---
+++ DiamanteX.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DiamanteX.ConsoleApp;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("================================");
+    Console.WriteLine("-------- Diamante de X ---------");
+    Console.WriteLine("================================");
+    Console.WriteLine();
+  }
+
+}
+
+
+
